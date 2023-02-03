e8bb052c5a93e0a736e624478ce0e36a8be05a20
hyemin <88538911+hhyem@users.noreply.github.com>
2023-02-03 23:24:20 +0900

    Create 2438.c

---
+++ 2438.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int num;
+  scanf("%d", &num);
+
+  for (int i = 0; i < num; i++) {
+    for (int j = 0; j < num; j++) {
+      if (i >= j) {
+        printf("*");
+    }
+  }
+
+  printf("\n");
+
+}
