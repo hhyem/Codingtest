91e8b30aefdfd1dd74a5c37d26bd1c51f0e8dbe7
hyemin <88538911+hhyem@users.noreply.github.com>
2024-03-01 01:01:22 +0900

    Create p_181850.c

---
+++ p_181850.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(double flo) {
+    int answer = 0;
+    
+    answer = flo;
+    
+    return answer;
+}
+
+int main() {
+    double f;
+    scanf("%lf", &f);
+    
+    solution(f);
+}
