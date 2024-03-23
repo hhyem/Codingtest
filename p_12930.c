a6b368244e25c52dd843f2a556c75cc5af55dc2d
hyemin <88538911+hhyem@users.noreply.github.com>
2024-03-23 17:57:01 +0900

    Update p_12930.c

--- p_12930.c
+++ p_12930.c
@@ -6,14 +6,28 @@
 char* solution(const char* s) {
     int size = strlen(s);
     char* answer = calloc(size, sizeof(char));
+    int tmp = 0, j = -1;
     
-    for(int i=0; i<size; i++) { 
-        if(i % 2 == 0)
-            answer[i] = s[i] - 32;
-        else
-            answer[i] = s[i];
+    char *ptr = strtok(s, " ");      // " " 공백 문자를 기준으로 문자열을 자름, 포인터 반환
+
+    while (ptr != NULL) {             // 자른 문자열이 나오지 않을 때까지 반복
+        j+=1;
+        for(int i=j; i<strlen(ptr)+j; i++) {
+            if(i % 2 == 0) 
+                answer[i] = s[i] - 32;
+            else if(i % 2 == 1) 
+                answer[i] = s[i];
+            tmp = i+1;
+        }
+        j = tmp;
+        if(j == size) 
+            break;
+        else 
+            answer[j] = ' '; //다음 공백 
+
+        ptr = strtok(NULL, " ");      // 다음 문자열을 잘라서 포인터를 반환
     }
-    
+
     return answer;
 }
 
@@ -21,5 +35,5 @@ int main() {
     char str;
     scanf("%s", &str);
     
-    solution(str);
+    solution(&str);
 }
