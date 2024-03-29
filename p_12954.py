5e84a3f148d50c34b06b990729eab8c51ac90a14
hyemin <88538911+hhyem@users.noreply.github.com>
2024-03-29 22:19:58 +0900

    Update p_12954.py

--- p_12954.py
+++ p_12954.py
@@ -4,7 +4,8 @@ def solution(x, n):
     answer = []
     #내가 짠 코드 문제점 : 런타임 에러 
     answer = [i for i in range(x,n*x+x,x)]
-    #https://1ets-just-do-it.tistory.com/29 참고 
+    #https://1ets-just-do-it.tistory.com/29 참고
+    #https://blog.naver.com/qbxlvnf11/221434003885 참고 
     #for문 변수는 횟수만 돌리고 계산은 i에서 한다. 1부터 n+1까지 돌린 후 
     #i와 x를 곱하면 된다.. 
     answer = [x*i for i in range(1, n+1)]
