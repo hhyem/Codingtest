cdc2b542d52f21ac0456d2deacd00e62431c5596
hyemin <88538911+hhyem@users.noreply.github.com>
2024-03-29 22:44:36 +0900

    Create p_12919.py

---
+++ p_12919.py
@@ -0,0 +1,7 @@
+def solution(seoul):
+    for i in range(len(seoul)): #배열 길이만큼
+        if seoul[i] == "Kim":
+            tmp = i
+    tmp = str(tmp)
+    answer = "김서방은 "+tmp+"에 있다"
+    return answer
